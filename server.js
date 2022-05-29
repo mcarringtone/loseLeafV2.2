6f1647ff53e95cac616a6c0017b1783f21e04402
Carrington <mcarringtone@gmail.com>
2022-05-28 21:51:53 -0400

    server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
